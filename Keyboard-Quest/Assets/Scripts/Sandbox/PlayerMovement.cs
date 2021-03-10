0bf37356fb6f26a85eadee11b56dbc335cf9ebbb
fk1g18 <fk1g18@soton.ac.uk>
2021-03-10 19:05:27 +0000

    Added a player and made the capable of moving

---
+++ Keyboard-Quest/Assets/Scripts/Sandbox/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D playerRigidBody;
+    private Animator animator;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        change = Vector3.zero;
+        change.x  = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("Moving", true);
+            
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        playerRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
