355e8ddac05abd4c60fefb786ebc49b8d027adf4
fk1g18 <fk1g18@soton.ac.uk>
2021-03-12 13:53:36 +0000

    Created the tilemap and made collision with walls

---
+++ Keyboard-Quest/Assets/Scripts/Sandbox/TitleChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleChanger : MonoBehaviour
+{
+    private GameObject player;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        text = GetComponent<Text>();
+        text.enabled = false;
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x < 0)
+        {
+            FadeTextToFullAlpha(1, text);
+            text.enabled = true;
+            //FadeTextToZeroAlpha(1, text);
+        }
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+ 
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
