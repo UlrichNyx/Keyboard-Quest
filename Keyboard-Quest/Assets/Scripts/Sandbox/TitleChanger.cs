c02c1d0d63dc62dbb9cf1dd54c3aa0a60cd9cd02
fk1g18 <fk1g18@soton.ac.uk>
2021-03-15 23:30:46 +0000

    Fixed title changing issue

--- Keyboard-Quest/Assets/Scripts/Sandbox/TitleChanger.cs
+++ Keyboard-Quest/Assets/Scripts/Sandbox/TitleChanger.cs
@@ -7,42 +7,50 @@ public class TitleChanger : MonoBehaviour
 {
     private GameObject player;
     private Text text;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         text = GetComponent<Text>();
         text.enabled = false;
+        timer = -4f;
         text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.x < 0)
+        if(player.transform.position.x < 0 && timer < 0 || timer < 0 && text.enabled)
         {
-            FadeTextToFullAlpha(1, text);
             text.enabled = true;
-            //FadeTextToZeroAlpha(1, text);
+            StartCoroutine(FadeTextToFullAlpha(10, text));
+            timer += Time.deltaTime;
+        }
+        else if(timer < 4 && timer >= 0)
+        {
+            StartCoroutine(FadeTextToZeroAlpha(10, text));
+            timer += Time.deltaTime;
         }
     }
 
-    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    public IEnumerator FadeTextToFullAlpha(float t, Text text)
     {
-        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
-        while (i.color.a < 1.0f)
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a);
+        while (text.color.a < 1.0f)
         {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / t));
             yield return null;
         }
     }
  
-    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    public IEnumerator FadeTextToZeroAlpha(float t, Text text)
     {
-        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
-        while (i.color.a > 0.0f)
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a);
+        while (text.color.a > 0.0f)
         {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / t));
             yield return null;
         }
     }
