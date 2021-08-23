3ad70bbe4fe0889a816b1becf8af265d64adf772
fk1g18 <fk1g18@soton.ac.uk>
2021-08-23 21:35:33 +0300

    More shop developing

--- Keyboard-Quest/Assets/Scripts/Sandbox/Interactable/Shop.cs
+++ Keyboard-Quest/Assets/Scripts/Sandbox/Interactable/Shop.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -9,9 +10,39 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject shopUI;
+    private GameObject[] itemButtons;
     public ItemListing[] itemsOnSale;
+    private Item highlighted;
+
+    public void ShowItemDescription(Item item)
+    {
+        highlighted = item;
+
+        Image itemIconDisplay = shopUI.transform.Find("ItemIcon").GetComponent<Image>();
+        Text itemNameDisplay = shopUI.transform.Find("ItemNameText").GetComponent<Text>();
+        Text itemDescriptionDisplay = shopUI.transform.Find("ItemDescriptionText").GetComponent<Text>();
+
+        itemIconDisplay.color = new Color32(255,255,255,255);
+        itemIconDisplay.sprite = item.icon;
+        itemNameDisplay.text = item.itemName;
+        itemDescriptionDisplay.text = item.description;
+    }
+
     public void ShowAffirmative()
     {
+        Transform list = shopUI.transform.Find("Scroll View").transform.Find("Viewport").transform.Find("Content");
+        int i = 0;
+        foreach (Transform child in list)
+        {
+            if(i < itemsOnSale.Length)
+            {
+                child.GetComponent<ItemRow>().item = itemsOnSale[i].item;
+                child.Find("Button").transform.Find("ItemText").GetComponent<Text>().text = itemsOnSale[i].item.itemName;
+                child.Find("Button").transform.Find("ItemImage").GetComponent<Image>().sprite = itemsOnSale[i].item.icon;
+                child.Find("Button").transform.Find("PriceText").GetComponent<Text>().text = itemsOnSale[i].price + "g";
+            }
+            i++;
+        }
         shopUI.SetActive(true);
     }
 
---
+++ Keyboard-Quest/Assets/Scripts/Sandbox/Inventory/ItemRow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRow : MonoBehaviour
+{
+    public Item item;
+
+    public void ShowItemDescription()
+    {
+        GameObject.Find("Shopkeeper").GetComponent<Shop>().ShowItemDescription(item);
+    }
+}
